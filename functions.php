f06805a2a7316076f996c0b454b4d0ab3c7499d8
janbegjan <janbeg2014@gmail.com>
2018-10-14 19:57:37 +0600

    assests categorized

--- functions.php
+++ functions.php
@@ -15,14 +15,12 @@ add_action('after_setup_theme', 'wptextual_bootstrapping');
 * wptextual theme assets
 */
 function wptextual_assets(){
-  // wp_enqueue_style( 'bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
+  // stylesheets
   wp_enqueue_style('font-awesome-css', get_theme_file_uri('/assets/css/font-awesome.min.css'), null);
   wp_enqueue_style('bootstrap-css', get_theme_file_uri('/assets/css/bootstrap.min.css'), null);
   wp_enqueue_style('wptextual_main-css', get_theme_file_uri('/assets/css/main.css'), null);
   wp_enqueue_style('wptextual_stylesheet-css', get_stylesheet_uri(), null, time());
-
-
-
+  //scripts
   wp_enqueue_script('wptextual-mainjs', get_theme_file_uri('/assets/js/main.js' ), array('jquery'), null, true);
   wp_enqueue_script('bootstrap-js', get_theme_file_uri('/assets/js/bootstrap.min.js'), array('jquery'), null, true);
 }
